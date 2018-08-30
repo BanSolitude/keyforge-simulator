5ee8e99df225d8dd8aaef1828b5443a850a8d31b
capocane <danielsbusto@gmail.com>
2018-08-30 16:25:59 -0400

    Actually test first turn functionality

--- tests/controllerTests.py
+++ tests/controllerTests.py
@@ -16,15 +16,15 @@ class ControllerTests(KeyforgeTest):
 
     def test_firstTurn_onlyOneCardPlayed(self):
         self.controller.first_turn()
-        #TODO test that one card is played
+        self.assertEqual(self.mockPlayer.cardsPlayed, 1)
 
     def test_firstTurn_extraCardDrawn(self):
         self.controller.first_turn()
-        #TODO how do I actually test this?
+        self.assertEqual(self.mockPlayer.state.cardsDrawn, 1)
 
     def test_takeTurn_selectsAppropriateHouse(self):
         self.controller.turn()
-        self.assertIn(self.mockPlayer.house, DECK_HOUSES)
+        self.assertIn(self.mockPlayer.activeHouse, DECK_HOUSES)
 
 class MockPlayer():
     def __init__(self):
@@ -36,7 +36,7 @@ class MockPlayer():
 
     def choose_house(self, house):
         self.called.add(Steps.CHOOSE)
-        self.house = house
+        self.activeHouse = house
 
     def ready_cards(self):
         self.called.add(Steps.READY)
@@ -47,9 +47,18 @@ class MockPlayer():
     def reset(self):
         self.called = set()
         self.cardsPlayed = 0
+        
+    def play_card(self, card):
+        self.cardsPlayed += 1
 
 class MockState():
     def __init__(self):
         self.deck = []
+        self.hand = []
+        self.cardsDrawn = 0
         for house in DECK_HOUSES:
             self.deck.append(Card(house))
+            self.hand.append(Card(house))        
+        
+    def draw(self, amount):
+        self.cardsDrawn += amount
