82359299f7a06161f4ae9ebd22d1322b677877b5
capocane <danielsbusto@gmail.com>
2018-08-31 11:29:26 -0400

    Run multiple rounds in simulator to figure out first player advantage

--- simulator.py
+++ simulator.py
@@ -2,6 +2,8 @@ from state import State
 from cards import Action, Creature, House
 from controller import MaxCardsController
 
+ROUNDS = 1000
+
 if __name__ == '__main__':
     deck = []
     for i in range(6):
@@ -21,30 +23,29 @@ if __name__ == '__main__':
         deckTwo.append(Creature(House.DIS))
         deckTwo.append(Creature(House.LOGOS))
 
-    gameState = State(deck, deckTwo)
-    controllers = [MaxCardsController(gameState.players[0]), MaxCardsController(gameState.players[1])]
+    wins = [0,0]
 
-    controllers[0].first_turn()
+    for i in range(ROUNDS):
+        gameState = State(deck, deckTwo)
+        controllers = [MaxCardsController(gameState.players[0]), MaxCardsController(gameState.players[1])]
 
-    activeController = 1
-    
-    while True:
-        print ("Battleline length = %d" % len(controllers[activeController].player.state.battleline))
-        print ("Deck length = %d" % len(controllers[activeController].player.state.deck))
-        print ("Hand length = %d" % len(controllers[activeController].player.state.hand))
+        controllers[0].first_turn()
 
-        controllers[activeController].turn()
+        activeController = 1
         
-        if controllers[activeController].player.state.keys >= 3:
-            break
+        while True:
+            controllers[activeController].turn()
+            
+            if controllers[activeController].player.state.keys >= 3:
+                break
 
-        print(len(controllers[activeController].player.state.battleline))
+            #print ('Active player has %d keys and %d amber' % (controllers[activeController].player.state.keys, controllers[activeController].player.state.amber))
 
-        print ('Active player has %d keys and %d amber' % (controllers[activeController].player.state.keys, controllers[activeController].player.state.amber))
+            activeController = (activeController + 1) % 2
 
-        activeController = (activeController + 1) % 2
+        if controllers[activeController].player == gameState.players[0]:
+            wins[0] += 1
+        else:
+            wins[1] += 1
 
-    if controllers[activeController].player == gameState.players[0]:
-        print('first player won')
-    else:
-        print('second player won')
+    print('First player won {} and second player won {}'.format(*wins))
